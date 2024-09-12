2da28b0a3912542a3821d9bd4afa6e388041795d
guts <artem.dev@list.ru>
2024-09-12 19:20:19 +0300

    Надо доработать страницу Проекты

--- template-parts/content-projects-slider.php
+++ template-parts/content-projects-slider.php
@@ -1,28 +1,41 @@
 <div class="slider">
 	<div class="slider__header">
 		<div class="slider__navigation">
-			<button class="slider__button-prev"><img src="/svg/arrow.svg" alt="Стрелка влево"></button>
-			<p class="slider__text"></p>
-			<button class="slider__button-next"><img src="/svg/arrow.svg" alt="Стрелка вправо"></button>
+			<button class="slider__button-prev"><img src="<?php bloginfo('template_url') ?>/assets/svg/arrow.svg" alt="Стрелка влево"></button>
+			<p class="slider__text">
+				<?php the_title() ?></p>
+			<button class="slider__button-next"><img src="<?php bloginfo('template_url') ?>/assets/svg/arrow.svg" alt="Стрелка вправо"></button>
 		</div>
 		<div class="slider__counter">
 			<div class="slider__counter-number"><span class="slider__current-count">1</span></div>
 			<div class="slider__counter-slash"><span>/</span></div>
-			<div class="slider__counter-number"><span>6</span></div>
+			<div class="slider__counter-number"><span><?php echo wp_count_posts('projects')->publish ?></span></div>
 		</div>
 	</div>
 	<div class="swiper">
 		<div class="swiper-wrapper">
-			<div class="swiper-slide"><img src="/pictures/slider/1.png"
-					alt="ОТЕЛЬ «Hilton Garden Inn Moscow Krasnoselskaya»"></div>
-			<div class="swiper-slide"><img src="/pictures/slider/2.png" alt="Универмаг «Цветной»"></div>
-			<div class="swiper-slide"><img src="/pictures/slider/3.png"
-					alt="Продовольственная розничная сеть «ДИКСИ»"></div>
-			<div class="swiper-slide"><img src="/pictures/slider/4.png" alt="Городская ферма «РусЭко»"></div>
-			<div class="swiper-slide"><img src="/pictures/slider/5.png"
-					alt="Продовольственная розничная сеть «ДИКСИ»"></div>
-			<div class="swiper-slide"><img src="/pictures/slider/6.png"
-					alt="Офисное лофт-здание в Даниловской мануфактуре"></div>
+			<?php
+			$args = array(
+				'post_type' => 'projects',
+				'posts_per_page' => -1
+			);
+
+			$query = new WP_Query($args);
+
+			if ($query->have_posts()):
+				while ($query->have_posts()) : $query->the_post();
+			?>
+					<div class="swiper-slide">
+
+						<?php the_post_thumbnail(); ?>
+					</div>
+			<?php
+				endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+			?>
 		</div>
 	</div>
 </div>
