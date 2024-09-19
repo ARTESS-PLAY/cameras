494f8eb44855e3d4588ba948ada334182577e390
pan0mera <artess.ponomarev@mail.ru>
2024-09-19 03:37:47 +0300

    feature(solutions) регистрация нового типа записи - типовое решение

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -9,12 +9,14 @@
 add_action('init', 'asbvideo_register_post_types');
 
 /*
-   Регистрация нового типа записи - Проекты
+ * Регистрация новый типов записи
+ * - Проекты
+ * - Типовые решения
 */
 
 function asbvideo_register_post_types()
 {
-	$args = [
+	$args_projects = [
 		'label'  => esc_html__('Проекты', 'asbvideo'),
 		'labels' => [
 			'name'               => esc_html__('Проекты', 'asbvideo'),
@@ -46,7 +48,41 @@ function asbvideo_register_post_types()
 		'show_in_rest'        => true,
 		'rest_base'           => true,
 	];
-	register_post_type('projects', $args);
+	register_post_type('projects', $args_projects);
+
+	$args_solutions = [
+		'label'  => esc_html__('Типовые решения', 'asbvideo'),
+		'labels' => [
+			'name'               => esc_html__('Типовые решения', 'asbvideo'),
+			'singular_name'      => esc_html__('Типовое решение', 'asbvideo'),
+			'add_new'            => esc_html__('Добавить типовое решение', 'asbvideo'),
+			'add_new_item'       => esc_html__('Добавление типового решения', 'asbvideo'),
+			'edit_item'          => esc_html__('Редактирование типового решения', 'asbvideo'),
+			'new_item'           => esc_html__('Новое типовое решение', 'asbvideo'),
+			'view_item'          => esc_html__('Смотреть типовое решение решение', 'asbvideo'),
+			'search_items'       => esc_html__('Искать типовое решение', 'asbvideo'),
+			'not_found'          => esc_html__('Не найдено', 'asbvideo'),
+			'not_found_in_trash' => esc_html__('Не найдено в корзине', 'asbvideo'),
+			'parent_item_colon'  => '',
+			'menu_name'          => esc_html__('Типовые решения'),
+		],
+		'description'            => esc_html__('Типовые решения'),
+		'public'              => true,
+		'publicly_queryable'  => false,
+		'query_var '  		  => false,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 4,
+		'menu_icon'           => 'dashicons-admin-generic',
+		'hierarchical'        => false,
+		'supports'            => ['title', 'excerpt', 'thumbnail'],
+		'has_archive'         => true,
+		'rewrite'             => true,
+		'query_var'           => true,
+		'show_in_rest'        => true,
+		'rest_base'           => true,
+	];
+	register_post_type('solutions', $args_solutions);
 }
 
 /*
